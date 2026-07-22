a1ea39a295ba28b8f016ad528c1c742464f33eee
K N Chiranthan <chiranthangowda41@gmail.com>
2026-07-22 17:24:31 +0530

    verified the token of user by secrey key if verifed then stored it in decoded and pushed to req.user body

--- Server/middlware/authMiddleware.js
+++ Server/middlware/authMiddleware.js
@@ -5,10 +5,22 @@ module.exports = async(req,res,next)=>{
    if(!authHeader)
    {
       return res.status(400).json({
-        message : "No token provided"
+         message:"No token seen"
       })
    }
    const token = authHeader.split(" ")[1];
-   console.log(token);
-   next();
+     console.log(token)
+     console.log(process.env.JWT_SECRET)
+   try{
+     const decoded = jwt.verify(token,process.env.JWT_SECRET)
+     console.log(decoded);
+     req.user=decoded
+     next();
+   }
+   catch(error){
+     console.log(error)
+     return res.status(400).json({
+        message:"Token has been corrupted"
+     })
+   }
 }
--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -6,6 +6,7 @@ const protect = require("../middlware/authMiddleware")
 router.post("/register",authController.register)
 router.post("/login",authController.login)
 router.get("/profile",protect,(req,res)=>{
+  console.log(req.user._id)
   res.send("Middleware is working")
 })
 
