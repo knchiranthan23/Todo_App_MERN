094485867ee1764a026179ab80204a150f327680
K N Chiranthan <chiranthangowda41@gmail.com>
2026-07-22 12:47:37 +0530

    hased user password with bcrypt and aswell created and test register API in postman

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,4 +1,5 @@
-const user = require("../models/user")
+const userModel = require("../models/user")
+const bcrypt = require('bcryptjs')
 
 exports.register = async(req,res)=>{
    const {name,email,password} = req.body;
@@ -8,4 +9,20 @@ exports.register = async(req,res)=>{
         message :"All fields are required"
       });
    }
+   const existingUser = await userModel.findOne({email})
+   if(existingUser){
+      return res.status(400).json({
+        message : "User already exits"
+      })
+   }
+   const hashedpassword = await bcrypt.hash(password,10);
+   const user = await userModel.create({
+      name,
+      email,
+      password : hashedpassword
+   })
+   return res.status(201).json({
+      message : "user registered successfully",
+      user
+   })
 }
