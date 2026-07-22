8635750e5560c41687103e2b0708c6148ef3f695
K N Chiranthan <chiranthangowda41@gmail.com>
2026-07-22 12:05:47 +0530

    Creater route folders and aauthRoutes file to store all the routes used in project

--- Server/app.js
+++ Server/app.js
@@ -3,11 +3,17 @@ require("dotenv").config();
 const connectDB = require("./config/db")
 
 const app = express();
+app.use(express.json());
+
+const userRoutes = require("./routes/authRoutes");
+app.use("/api/auth",userRoutes)
 
 app.get("/",(req,res)=>{
     res.send("Hello Clients this from server side");
 })
+
 connectDB();
+
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT,()=>{
     console.log(`Server is successfully running in port ${PORT}`)
